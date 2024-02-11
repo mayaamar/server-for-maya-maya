f7f2fddb998ec726e79f968ba42189bda7df582f
Bar Roth <baroth2005@gmail.com>
2024-02-11 17:04:49 +0200

    feat(): change server to match my computer (change ip to loclhost and remove shraga)

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import config from "./utils/config";
 
 mongoose
     .connect(config.MONGO_URL)
-    .then(() => app.listen(config.port, "10.100.11.131" || "localhost" , () => console.log(`app running on port ${config.port}`)))
+    .then(() => app.listen(config.port, () => console.log(`app running on port ${config.port}`)))
     .catch((err) => console.log(err));
 
 export default app;
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,7 +4,7 @@ export default {
     port: env.get("PORT").default(3000).asPortNumber(),
     MONGO_URL: env.get("MONGOOSE_URL").default("mongodb://localhost:27017/shop").asString(),
     shragaUrl: "https://shraga.shraga.branch-yesodot.org",
-    callbackURL: "http://10.100.11.131:3000/auth/callback",
+    callbackURL: "http://localhost:3000/auth/callback",
     secret: "secret",
     useEnrichId: false,
 };
